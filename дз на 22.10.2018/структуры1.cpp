c929525e0d7a59ca3aab1c5a3499bcc98b94db46
serg-shkviro <serg200211@mail,ru>
2018-10-21 22:57:55 +0300

    Update дз на 22.10.2018/структуры1.cpp

--- дз на 22.10.2018/структуры1.cpp
+++ дз на 22.10.2018/структуры1.cpp
@@ -11,6 +11,11 @@ struct rational {
 		reduction();
 	}
 
+	rational(rational* other) {
+		numerator = other->numerator;
+		denominator = other->denominator;
+	}
+
 	void reduction() {
 		int ab = nod(numerator, denominator);
 		numerator = numerator / ab;
@@ -53,13 +58,10 @@ struct rational {
 
 	int comp(rational b)
 	{
-		rational* c = new rational (numerator,denominator);
+		rational* c = new rational(numerator, denominator);
 		c->sub(b);
 
-
-
-
-		if (c->numerator > 0 && c->denominator < 0) 
+		if (c->numerator > 0 && c->denominator < 0)
 		{
 			return -1;
 		}
@@ -82,83 +84,79 @@ struct rational {
 		}
 		return 0;
 	}
-
 };
 
-	
+istream& operator>> (istream& stream, rational& rat)
+{
+	stream >> rat.numerator >> rat.denominator;
+	return stream;
+}
 
-	int main() {
-		int x1, y1, x2, y2;
-
-		cin >> x1;
-		cin >> y1;
-		cin >> x2;
-		cin >> y2;
-
-		rational* a = new rational(x1, y1);
-		rational* b = new rational(x2, y2);
-		a->mult(*b);
-		cout << " mult " << endl;
-		cout << a->numerator << "|" << a->denominator << endl;
-
-		a = new rational(x1, y1);
-		b = new rational(x2, y2);
-		a->sum(*b);
-		cout << " sum " << endl;
-		cout << a->numerator << " | " << a->denominator << endl;
+ostream& operator<< (ostream& stream, rational& rat)
+{
+	stream << rat.numerator << "/" << rat.denominator;
+	return stream;
+}
 
 
-		a = new rational(x1, y1);
-		b = new rational(x2, y2);
-		a->sub(*b);
-		cout << " sub " << endl;
-		cout << a->numerator << " | " << a->denominator << endl;
+int main() {
+	rational* x = new rational(0, 1);
+	rational* y = new rational(0, 1);
 
+	cin >> *x;
+	cin >> *y;
 
-		a = new rational(x1, y1);
-		b = new rational(x2, y2);
-		a->div(*b);
-		cout << " div " << endl;
-		cout << a->numerator << " | " << a->denominator << endl;
+	rational* a = new rational(x);
+	rational* b = new rational(y);
+	a->mult(*b);
+	cout << " mult " << endl;
+	//cout << a->numerator << "|" << a->denominator << endl;
+	cout << *a << endl;
 
-		a = new rational(x1, y1);
-		b = new rational(x2, y2);
-		a->comp(*b);
-		cout << " comp " << endl;
-		cout << a->comp(*b);
+	a = new rational(x);
+	b = new rational(y);
+	a->sum(*b);
+	cout << " sum " << endl;
+	//cout << a->numerator << " | " << a->denominator << endl;
+	cout << *a << endl;
 
+	a = new rational(x);
+	b = new rational(y);
+	a->sub(*b);
+	cout << " sub " << endl;
+	//cout << a->numerator << " | " << a->denominator << endl;
+	cout << *a << endl;
+
+	a = new rational(x);
+	b = new rational(y);
+	a->div(*b);
+	cout << " div " << endl;
+	//cout << a->numerator << " | " << a->denominator << endl;
+	cout << *a << endl;
+
+	a = new rational(x);
+	b = new rational(y);
+	a->comp(*b);
+	cout << " comp " << endl;
+	cout << a->comp(*b);
+}
+
+int nod(int a, int b)
+{
+	if (a < b) {
+		swap(a, b);
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	int nod(int a, int b)
-	{
-		if (a < b) {
-			swap(a, b);
-		}
-		if (b == 0) {
-			return 1;
-		}
-		int q = a / b;
-		int r = a - b * q;
-		int r1 = b;
-		while (r > 0) {
-			r1 = r;
-			q = b / r;
-			r = b - r * q;
-			b = r1;
-		}
-		return r1;
+	if (b == 0) {
+		return 1;
 	}
+	int q = a / b;
+	int r = a - b * q;
+	int r1 = b;
+	while (r > 0) {
+		r1 = r;
+		q = b / r;
+		r = b - r * q;
+		b = r1;
+	}
+	return r1;
+}
