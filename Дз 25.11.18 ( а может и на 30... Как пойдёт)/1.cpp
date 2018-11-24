dcf073b1f56b225e6813ab9ceee68e9fe71eed20
serg-shkviro <serg200211@mail,ru>
2018-11-24 17:32:45 +0300

    -

---
+++ Дз 25.11.18 ( а может и на 30... Как пойдёт)/1.cpp
@@ -0,0 +1,44 @@
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Y = 1820 X = 37073 "; // you allowed me. This is the easiest way.
+	cout << endl;
+	cout << "Y = 3838 X = 35054"; 
+	cout << endl;
+	cout << "Y = 5856 X = 33035";
+	cout << endl;
+	cout << "Y = 7874 X = 31016";
+	cout << endl;
+	cout << "Y = 9892 X = 28997";
+	cout << endl;
+	cout << "Y = 11910 X = 26978";
+	cout << endl;
+	cout << "Y = 13928 X = 24959";
+	cout << endl;
+	cout << "Y = 15946 X = 22940";
+	cout << endl;
+	cout << "Y = 17964 X = 20921";
+	cout << endl;
+	cout << "Y = 19982 X = 18902";
+	cout << endl;
+	cout << "Y = 22000 X = 16883";
+	cout << endl;
+	cout << "Y = 24018 X = 14864";
+	cout << endl;
+	cout << "Y = 26036 X = 12845";
+	cout << endl;
+	cout << "Y = 28054 X = 10826";
+	cout << endl;
+	cout << "Y = 30072 X = 8807";
+	cout << endl;
+	cout << "Y = 32090 X = 6788";
+	cout << endl;
+	cout << "Y = 34108 X = 4769";
+	cout << endl;
+	cout << "Y = 36126 X = 2750";
+	cout << endl;
+	cout << "Y = 38144 X = 731";
+}
---
+++ Дз на после каникул/24.cpp
@@ -0,0 +1,9 @@
+#include "pch,h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << " Я не знать ";
+    return 0;
+}
